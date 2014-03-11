4948d2f69a9fbfb0cf419fbeb4738bba4e38ed9f
Ikuo Kirihara <equo.keeley@gmail.com>
2014-03-11 22:23:52 +0900

    to p.37

--- MF_Refactoring_FirstExample/Movie.cs
+++ MF_Refactoring_FirstExample/Movie.cs
@@ -31,5 +31,47 @@ namespace MF_Refactoring_FirstExample
         {
             get { return title; }
         }
+
+        public double GetCharge(int daysRented)
+        {
+            double result = 0;
+
+            switch (PriceCode)
+            {
+                case Movie.Regular:
+                    result += 2;
+                    if (daysRented > 2)
+                    {
+                        result += (daysRented - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NewRelease:
+                    result += daysRented * 3;
+                    break;
+                case Movie.Childrens:
+                    result += 1.5;
+                    if (daysRented > 3)
+                    {
+                        result += (daysRented - 3) * 1.5;
+                    }
+                    break;
+            }
+
+            return result;
+        }
+
+        public int GetFrequentRenterPoints(int daystRented)
+        {
+            // Give bonus points if rented "new release" over two days
+            if (PriceCode == Movie.NewRelease &&
+                daystRented > 1)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
     }
 }
--- MF_Refactoring_FirstExample/Rental.cs
+++ MF_Refactoring_FirstExample/Rental.cs
@@ -29,44 +29,12 @@ namespace MF_Refactoring_FirstExample
 
         public double GetCharge()
         {
-            double result = 0;
-
-            switch (Movie.PriceCode)
-            {
-                case Movie.Regular:
-                    result += 2;
-                    if (DaysRented > 2)
-                    {
-                        result += (DaysRented - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NewRelease:
-                    result += DaysRented * 3;
-                    break;
-                case Movie.Childrens:
-                    result += 1.5;
-                    if (DaysRented > 3)
-                    {
-                        result += (DaysRented - 3) * 1.5;
-                    }
-                    break;
-            }
-
-            return result;
+            return Movie.GetCharge(DaysRented);
         }
 
         public int GetFrequentRenterPoints()
         {
-            // Give bonus points if rented "new release" over two days
-            if (Movie.PriceCode == Movie.NewRelease && 
-                DaysRented > 1)
-            {
-                return 2;
-            }
-            else
-            {
-                return 1;
-            }
+            return Movie.GetFrequentRenterPoints(daysRented);
         }
     }
 }
